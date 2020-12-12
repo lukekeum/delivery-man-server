d8666727db232d45405b2153fa5c3818737079f2
lukekeum <lukekeum11@gmail.com>
2020-12-13 02:25:53 +0900

    Remove cors from app

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,10 @@
 import express from 'express';
-import cors from 'cors';
 import bodyParser from 'body-parser';
-import morgan from 'morgan';
 import router from './routes';
 
 const app = express();
 
-app.use('*', cors);
 app.use(bodyParser.json());
-app.use(morgan('dev'));
 
 app.use(router);
 
