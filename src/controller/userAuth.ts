12013738edbfc9ef13508322ca7c167a3532e9a9
lukekeum <lukekeum11@gmail.com>
2020-12-13 02:25:35 +0900

    Add Login function

--- src/controller/userAuth.ts
+++ src/controller/userAuth.ts
@@ -2,11 +2,19 @@ import { getRepository } from 'typeorm';
 import User from '../entities/User';
 import UserProfile from '../entities/UserProfile';
 import Router from '../lib/router';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
 
 interface IRegisterInput {
   email: string;
   username: string;
   phone_number: string;
+  password: string;
+}
+
+interface ILoginInput {
+  username: string;
   password: string;
 }
 
@@ -31,12 +39,12 @@ export const register: Router<void> = async (req, res) => {
     if (Object.keys(errors).length > 0) return res.status(400).json({ errors });
 
     const user = new User({ email, username, phone_number, password });
+    await userRepo.save(user);
+
     const userProfile = new UserProfile({
       display_name: username,
       user_id: user.id,
     });
-
-    await userRepo.save(user);
     await userProfileRepo.save(userProfile);
 
     // Return User
@@ -48,12 +56,46 @@ export const register: Router<void> = async (req, res) => {
 };
 
 export const login: Router<void> = async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password }: ILoginInput = req.body;
 
   try {
-    // TODO: Validate Data
-    // TODO: Compare Information
-    // TODO: Return User and Token
+    const errors: any = {};
+    const userRepo = await getRepository(User);
+
+    // Validate Data
+    const usernameUser = await userRepo.findOne({ username });
+
+    if (!usernameUser) errors.username = 'Incorrected username';
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      usernameUser!.password
+    );
+
+    if (!isPasswordCorrect) errors.password = 'Incorrected Password';
+
+    // Return User and Token
+    if (Object.keys(errors).length > 0) return res.status(400).json({ errors });
+
+    const { JWT_SECRET = 'JWT_SECRET' } = process.env;
+
+    const token = jwt.sign({ username }, JWT_SECRET);
+
+    res.set(
+      'Set-Cookie',
+      cookie.serialize('token', token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+        maxAge: 60 * 60,
+        path: '/',
+      })
+    );
+
+    return res.json({
+      user: usernameUser,
+      token: token,
+    });
   } catch (err) {
     console.error(err);
     return res.status(500).json(err);
