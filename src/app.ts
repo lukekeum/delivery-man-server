5a0457d4c81dffa34c7f77146fc45a5b42b88a55
lukekeum <lukekeum11@gmail.com>
2020-12-12 16:52:07 +0900

    Remove apollo-server and use it with RESTApi

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,8 @@
 import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
 import cors from 'cors';
-import schema from './graphql/schema';
 
 const app = express();
-const server = new ApolloServer({
-  schema,
-  context: async ({ req, res }) => ({ req, res }),
-});
 
-app.use('*', cors());
-
-server.applyMiddleware({ app, path: '/graphql' });
+app.use('*', cors);
 
 export default app;
