a0fd99beb420839d5a3f31fc869498d4eadf7744
lukekeum <lukekeum11@gmail.com>
2020-12-12 17:00:16 +0900

    Add middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use('*', cors);
+app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 export default app;
