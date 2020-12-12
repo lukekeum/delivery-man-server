a57c26e9c005dfb56eae32f7a2d28ba4d3a5b627
lukekeum <lukekeum11@gmail.com>
2020-12-12 22:45:42 +0900

    Remove question mark from TRouter

--- src/lib/router.d.ts
+++ src/lib/router.d.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
 
-type TRouter<T = any> = (req: Request, res: Response, next?: NextFunction) => T;
+type TRouter<T = any> = (req: Request, res: Response, next: NextFunction) => T;
 
 export default TRouter;
