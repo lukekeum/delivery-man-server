c2a39d8519a6bee0cbcad173bbf327514f5a167e
lukekeum <lukekeum11@gmail.com>
2020-12-13 12:32:40 +0900

    Add cookie-parser in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import router from './routes';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.use(router);
 
